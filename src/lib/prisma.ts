95bcc3c2fad7430ec99410dcd9f2277949889119
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-04-07 18:38:59 -0300

    feat: Update prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../prisma/generated/prisma-client-js'
 
-export const prismaClient = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+  errorFormat: 'pretty',
+})
+
+export default prisma
