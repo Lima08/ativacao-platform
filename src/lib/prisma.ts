7c99caf640129527671089201c904a65627e39c8
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-04-07 23:20:01 -0300

    config(prisma): Update prisma configurations

--- src/lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '../../prisma/generated/prisma-client-js'
-
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-  errorFormat: 'pretty',
-})
-
-export default prisma
