2e2e5ed6df69117535204ba105138214ece95068
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-04-23 10:47:14 -0300

    chore: campaigns with mocked add + delete features

--- src/app/in/campaigns/page.tsx
+++ src/app/in/campaigns/page.tsx
@@ -1,10 +1,22 @@
+'use client'
+import ListWrapper2 from 'components/ListWrapper2'
 import PageContainer from 'components/PageContainer'
+import React, { useState } from 'react'
+import { CampaignsContext } from '../../../../context'
 
-export default async function Campaigns() {
+export default function Campaigns() {
+  const [campaingsList, setCampaingsList] = useState([])
+
   return (
-    <>
+    <CampaignsContext.Provider
+      value={{ state: campaingsList, setState: setCampaingsList }}
+    >
       <title>Ativação TEC | Campanhas</title>
-      <PageContainer pageTitle="Campanhas" buttonTitle="campanha" />
-    </>
+      <PageContainer pageTitle="Campanhas" buttonTitle="campanha">
+        <ListWrapper2 pageTitle="campanha" />
+        {/* <ListItem itemTitle="Campanha Fashion SP 2023" />
+          <ListItem itemTitle="Campanha Nike Air Jordan 2024" /> */}
+      </PageContainer>
+    </CampaignsContext.Provider>
   )
 }
