f1cf885881e545ad28e812595ed4f0f6b9a0e036
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-04-23 10:43:36 -0300

    feat: add page container component

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,22 +1,49 @@
+'use client'
+import { ReactNode, useContext } from 'react'
+import { CampaignsContext } from '../../../context'
+
 type PageContainerProps = {
-  pageTitle: string
   buttonTitle: string
+  children: ReactNode
+  pageTitle: string
 }
 
-function PageContainer({ pageTitle, buttonTitle }: PageContainerProps) {
+function PageContainer({
+  pageTitle,
+  buttonTitle,
+  children
+}: PageContainerProps) {
+  const { state, setState } = useContext(CampaignsContext)
+
+  function addItem() {
+    const item = {
+      elementId: (Math.random() * 100).toFixed(1),
+      itemTitle: `Campanha ${state.length + 1}`,
+      itemDescription:
+        'Campanha destinada a vender produtos da marca Y no verão de 2024'
+    }
+
+    const nextState = [...state, item]
+    setState(nextState)
+  }
+
   return (
     <div className="w-full">
-      <div className="flex p-[50px] items-center justify-around">
-        <h1 className="text-2xl font-medium">{pageTitle}</h1>
-        <button className="hover:bg-[#ffd700] bg-gray-100 p-4 text-black font-medium rounded-md">
-          Adicionar {buttonTitle}
-        </button>
+      <div className="flex items-center flex-col justify-around">
+        <div className="w-full flex p-[25px] items-center justify-around">
+          <h1 className="text-2xl font-medium">{pageTitle}</h1>
+          <button
+            className="bg-[#ffd700] hover:bg-gray-100 p-3 border border-slate-200 text-black font-medium rounded-md"
+            onClick={addItem}
+          >
+            Adicionar {buttonTitle}
+          </button>
+        </div>
+
+        <div className="w-full flex flex-col">{children}</div>
       </div>
     </div>
   )
 }
-
-// align-items: center;
-//     justify-content: space-around;
 
 export default PageContainer
