c4e1fb975fa3a63809551914cd50ab41adb55572
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-04-23 21:56:25 -0300

    chore: add mediaViewer component (WIP)

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,5 +1,7 @@
-import { useContext } from 'react'
-import { CampaignsContext } from '../../../context'
+'use client'
+import { useContext, useState } from 'react'
+import { CampaignsContext, INewItem } from '../../../context'
+import Modal from 'components/MediaViewer'
 
 type ListItemProps = {
   elementId: number
@@ -20,6 +22,8 @@ function ListItem({
   imgAlt = 'Campaign image'
 }: ListItemProps) {
   const { state, setState } = useContext(CampaignsContext)
+  const [itemView, setItemView] = useState<INewItem>()
+  const [open, setOpen] = useState(true)
 
   function remove(value: any) {
     const userDecision = confirm('Confirmar deleção?')
@@ -32,9 +36,24 @@ function ListItem({
     setState(nextState)
   }
 
+  function openMediaViewer(value: any) {
+    const element = state.find((element) => element.elementId === value.id)
+    if (element) setItemView(element)
+    setOpen(true)
+  }
+
   return (
     <tr>
       <td className="px-4 py-4 text-sm whitespace-nowrap">
+        {itemView && (
+          <Modal
+            title={itemView.itemTitle}
+            description={itemView.itemDescription}
+            imageSource={imgSrc}
+            open={open}
+            setOpen={setOpen}
+          />
+        )}
         <div className="flex items-center flex-col">
           <img
             className="object-cover w-12 h-12 -mx-1 border-2 border-white rounded-full dark:border-gray-700"
@@ -46,12 +65,15 @@ function ListItem({
 
       <td className="px-4 py-4 text-sm font-medium whitespace-nowrap">
         <div>
-          <h2 className="font-medium text-gray-800 dark:text-white ">
-            {itemTitle}
-          </h2>
-          {/* <p className="text-sm font-normal text-gray-600 dark:text-gray-400">
-            secondary title
-          </p> */}
+          <a
+            id={`${elementId}`}
+            href="#"
+            onClick={(e) => openMediaViewer(e.currentTarget)}
+          >
+            <h2 className="font-medium text-gray-800 dark:text-white ">
+              {itemTitle}
+            </h2>
+          </a>
         </div>
       </td>
 
@@ -60,32 +82,10 @@ function ListItem({
           <h4 className="text-gray-700 dark:text-gray-200">
             {itemDescription}
           </h4>
-          {/* <p className="text-gray-500 dark:text-gray-400">
-            secondary description
-          </p> */}
         </div>
       </td>
 
       <td className="px-4 py-4 text-sm whitespace-nowrap flex items-center justify-evenly">
-        {/* <button
-          className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100"
-          onClick={handleClick}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-            />
-          </svg>
-        </button> */}
         <div className="rounded px-1 py-1 text-gray-500 flex w-[40%] gap-2 flex-grow-[2]">
           <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 disabled">
             Editar
