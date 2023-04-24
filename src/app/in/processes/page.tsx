7788d655c242f97835080b9cede474d8e72286bb
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-04-24 17:01:46 -0300

    fix: erase useless code

--- src/app/in/processes/page.tsx
+++ src/app/in/processes/page.tsx
@@ -1,14 +1,3 @@
 export default async function Processes() {
-  return (
-    <div className="container mx-auto flex flex-col items-center justify-center">
-      <Suspense
-        fallback={
-          <div>
-            <h1>Carregando...</h1>
-          </div>
-        }
-      ></Suspense>
-      <h1>Processos</h1>
-    </div>
-  )
+  return <h1>Processos</h1>
 }
