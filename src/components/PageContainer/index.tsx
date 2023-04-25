9acef1f843392107237b93d78e727fc16e3ace56
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-04-25 13:44:20 -0300

    saving...

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,17 +1,31 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import { ReactNode } from 'react'
 
 type PageContainerProps = {
   children: ReactNode
   pageTitle: string
+  pageSection: string
 }
+export default function PageContainer({
+  pageTitle,
+  pageSection,
+  children
+}: PageContainerProps) {
+  const router = useRouter()
 
-function PageContainer({ pageTitle, children }: PageContainerProps) {
+  function navToCreatePage() {
+    router.push(`/in/${pageSection}/new`)
+  }
+
   return (
     <div className="w-full">
       <div className="w-full flex p-[25px] items-center justify-around">
         <h1 className="text-2xl font-medium">{pageTitle}</h1>
-        <button className="bg-[#ffd700] hover:bg-gray-100 p-3 border border-slate-200 text-black font-medium rounded-md">
+        <button
+          className="bg-[#ffd700] hover:bg-gray-100 p-3 border border-slate-200 text-black font-medium rounded-md"
+          onClick={navToCreatePage}
+        >
           Adicionar
         </button>
       </div>
@@ -20,5 +34,3 @@ function PageContainer({ pageTitle, children }: PageContainerProps) {
     </div>
   )
 }
-
-export default PageContainer
