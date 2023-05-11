feb4f049d57f69eb41910de55370fcbe5561ce7a
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-05-11 11:57:51 -0300

    refactor: Reset database and add varchars configurations

--- prisma/migrations/20230505203819_add_status_at_campaigns/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `campaigns` ADD COLUMN `active` BOOLEAN NOT NULL DEFAULT true;
--- prisma/migrations/20230511145540_init/migration.sql
+++ prisma/migrations/20230511145540_init/migration.sql
@@ -5,10 +5,11 @@ CREATE TABLE `users` (
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `companyId` VARCHAR(191) NOT NULL,
-    `imageUrl` VARCHAR(191) NULL,
+    `imageUrl` VARCHAR(255) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -17,7 +18,7 @@ CREATE TABLE `companies` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
-    `imageUrl` VARCHAR(191) NULL,
+    `imageUrl` VARCHAR(255) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -28,8 +29,9 @@ CREATE TABLE `companies` (
 -- CreateTable
 CREATE TABLE `media` (
     `id` VARCHAR(191) NOT NULL,
-    `url` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(255) NOT NULL,
     `type` ENUM('image', 'video') NOT NULL,
+    `key` VARCHAR(191) NOT NULL,
     `campaignId` VARCHAR(191) NULL,
     `trainingId` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -43,7 +45,8 @@ CREATE TABLE `media` (
 CREATE TABLE `trainings` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NULL,
+    `active` BOOLEAN NOT NULL DEFAULT true,
+    `description` VARCHAR(255) NULL,
     `userId` VARCHAR(191) NOT NULL,
     `companyId` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -56,9 +59,36 @@ CREATE TABLE `trainings` (
 CREATE TABLE `campaigns` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NULL,
+    `active` BOOLEAN NOT NULL DEFAULT true,
+    `description` VARCHAR(255) NULL,
     `userId` VARCHAR(191) NOT NULL,
     `companyId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_training` (
+    `id` VARCHAR(191) NOT NULL,
+    `trainingId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `status` ENUM('started', 'finished') NOT NULL DEFAULT 'started',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `analyzes` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `bucketUrl` VARCHAR(255) NULL,
+    `biUrl` VARCHAR(255) NULL,
+    `status` ENUM('pending', 'rejected', 'done') NOT NULL DEFAULT 'pending',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -85,3 +115,12 @@ ALTER TABLE `campaigns` ADD CONSTRAINT `campaigns_userId_fkey` FOREIGN KEY (`use
 
 -- AddForeignKey
 ALTER TABLE `campaigns` ADD CONSTRAINT `campaigns_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `companies`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_training` ADD CONSTRAINT `user_training_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_training` ADD CONSTRAINT `user_training_trainingId_fkey` FOREIGN KEY (`trainingId`) REFERENCES `trainings`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `analyzes` ADD CONSTRAINT `analyzes_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
