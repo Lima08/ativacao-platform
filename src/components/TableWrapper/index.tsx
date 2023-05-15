3af12314ee4a907724fbc2d929f0e42155444618
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-05-15 07:54:47 -0300

    refactor: Change prop from table

--- src/components/TableWrapper/index.tsx
+++ src/components/TableWrapper/index.tsx
@@ -97,7 +97,7 @@ export default function TableWrapper({
                                   <ToggleInput
                                     defaultActive={active}
                                     toggleId={id}
-                                    // onToggle={() => toggleActivation(id)}
+                                    onClickToggle={() => toggleActivation(id)}
                                   />
                                 )}
                                 {!!onEdit && (
