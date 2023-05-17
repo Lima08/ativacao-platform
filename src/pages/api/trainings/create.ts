5f35f45759e776d59be707e0ec4ecd12235f4b86
Lima08 <joaopaulo.gomeslima8@gmail.com>
2023-05-17 16:59:39 -0300

    refactor: Test homolog without middleware training route

--- src/pages/api/trainings/create.ts
+++ src/pages/api/trainings/create.ts
@@ -1,18 +1,17 @@
-import handler from 'handler'
 import { createTraining } from 'useCases/trainings'
 
 // TODO: Colocar middleware de validação
-export default handler.post(async (req, res) => {
+export default async function handler(req: any, res: any) {
   const { name, description, mediaIds } = req.body
-  const companyId = req.companyId!
-  const userId = req.userId!
 
-  const createdTraining = await createTraining({
-    name,
-    description,
-    userId,
-    companyId,
-    mediaIds
-  })
-  return res.status(201).json({ data: createdTraining })
-})
+  if (req.method === 'POST') {
+    const createdTraining = await createTraining({
+      name,
+      description,
+      userId: '4181b23f-c4a8-47d1-99c8-2db883d84eb3',
+      companyId: '5c9e558a-1eb8-44d4-9abb-693c65ee57c4',
+      mediaIds
+    })
+    return res.status(201).json({ data: createdTraining })
+  }
+}
