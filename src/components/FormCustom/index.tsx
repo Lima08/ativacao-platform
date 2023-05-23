66da029e1d0ff44be8e3a2b0db64b14dc5a6b155
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-05-23 15:56:17 -0300

    refactor: add customStyles to FormCustom component

--- src/components/FormCustom/index.tsx
+++ src/components/FormCustom/index.tsx
@@ -4,15 +4,20 @@
 type FormCustomProps = {
   children: React.ReactNode
   submitForm: (event: any) => void
+  customStyles?: string
 }
 
 export default function CampaignRegister({
   children,
-  submitForm
+  submitForm,
+  customStyles
 }: FormCustomProps) {
   return (
     <form
-      className="border-grey bg-white p-5 rounded-lg mt-5 mb-5 w-10/12 mx-auto"
+      className={
+        customStyles ||
+        'border-grey bg-white p-5 rounded-lg mt-5 mb-5 w-10/12 mx-auto'
+      }
       onSubmit={submitForm}
     >
       {children}
